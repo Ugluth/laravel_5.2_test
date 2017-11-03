c52868b533d8057195f8364ffa5364e6a28cda8b
Fotis <fotis@oakyapp.com>
2017-11-03 13:52:34 +0100

    Assigning the user provider on the middleware.

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -4,6 +4,10 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Auth\EloquentUserProvider;
+
+use App\Admin;
+use App\User;
 
 class Authenticate
 {
@@ -17,6 +21,23 @@ class Authenticate
      */
     public function handle($request, Closure $next, $guard = null)
     {
+        if (empty($guard)) {
+
+        } else if ($guard === 'app') {
+            config('auth.defaults.guard', 'app');
+            config('auth.defaults.passwords', 'users');
+
+            $user_provider = new EloquentUserProvider(app('hash'), User::class);
+            app()->auth->setProvider($user_provider);
+
+        } else if ($guard === 'admin') {
+            config('auth.defaults.guard', 'admin');
+            config('auth.defaults.passwords', 'admins');
+
+            $user_provider = new EloquentUserProvider(app('hash'), Admin::class);
+            app()->auth->setProvider($user_provider);
+        }
+
         if (Auth::guard($guard)->guest()) {
             if ($request->ajax() || $request->wantsJson()) {
                 return response('Unauthorized.', 401);
